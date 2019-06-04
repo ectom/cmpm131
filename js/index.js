807b55cfde7c84c60ce525ed23fe356c93653d0f
Ethan Tom <etom579@gmail.com>
2019-06-03 23:26:08 -0700

    favoriting works

--- js/index.js
+++ js/index.js
@@ -83,7 +83,7 @@ function searchEvents() {
 
 function addPost(post){
     var div = document.createElement('div');
-    div.className = "post"
+    div.className = "post module"
 
     if(post["postType"] == "deal") {
         numDeals++;
@@ -91,33 +91,37 @@ function addPost(post){
             <div class='post_title'>
                 <h3>`+ post['title'] +`</h3>
                 <div class='clickables'>
-                    <img class='verify' src='./pics/fav.png' alt=''>
-                    <span id='likes`+ numDeals +`'>0</span>
-                    <button onclick='upVote(`+ numDeals +`)'><img class='thumb' src='./pics/Like.png' alt=''></button>
-                    <span id='dislikes`+ numDeals +`'>0</span>
-                    <button type='button' name='button' onclick='downVote(`+ numDeals +`)'><img class='thumb' src='./pics/Dislike.png' alt=''></button>
+                    <a role="button" onclick="dfavorite(`+ numDeals +`)"><img id="dfavorite`+ numDeals +`" class='favorite' src='./pics/fav.png' alt=''></a>
+                    <span id='dLikes`+ numDeals +`'>0</span>
+                    <button onclick='dUpVote(`+ numDeals +`)'><img class='thumb' src='./pics/Like.png' alt=''></button>
+                    <span id='dDislikes`+ numDeals +`'>0</span>
+                    <button type='button' name='button' onclick='dDownVote(`+ numDeals +`)'><img class='thumb' src='./pics/Dislike.png' alt=''></button>
+                    <a role="button" class="collapsed" data-toggle="collapse" href="#dcollapse`+ numDeals +`" aria-expanded="false" onclick="dcolspand(`+ numDeals +`)" aria-controls="collapseExample"><img id="dexpand`+ numDeals +`" src="./pics/collapse.png" alt=""></a>
                 </div>
             </div>
-            <p>`+ post['description'] +`</p>
-
-            <p>ENDS: `+ post['endDate'] +`<br>COST: `+ post['cost'] +`<br> LOCATION: `+ post['location'] +` <br>TAGS: `+ post['tags'] +`</p>`;
+            <span class="collapse" id="dcollapse`+ numDeals +`" aria-expanded="false">
+                <p>`+ post['description'] +`</p>
+                <p>ENDS: `+ post['endDate'] +`<br>COST: `+ post['cost'] +`<br> LOCATION: `+ post['location'] +` <br>TAGS: `+ post['tags'] +`</p>
+            </span>`;
             document.getElementById('dealUL').appendChild(div);
     } else {
         numEvents++;
         div.innerHTML = `
             <div class='post_title'>
                 <h3>`+ post['title'] +`</h3>
-                <div class=''clickables''>
-                    <img class='verify' src='./pics/fav.png' alt=''>
-                    <span id='likes`+ numEvents +`'>0</span>
-                    <button onclick='upVote(`+ numEvents +`)'><img class='thumb' src='./pics/Like.png' alt=''></button>
-                    <span id='dislikes`+ numEvents +`'>0</span>
-                    <button type='button' name='button' onclick='downVote(`+ numEvents +`)'><img class='thumb' src='./pics/Dislike.png' alt=''></button>
+                <div class='clickables'>
+                    <a role="button" onclick="efavorite(`+ numEvents +`)"><img id="efavorite`+ numEvents +`" class='favorite' src='./pics/fav.png' alt=''></a>
+                    <span id='eLikes`+ numEvents +`'>0</span>
+                    <button onclick='eUpVote(`+ numEvents +`)'><img class='thumb' src='./pics/Like.png' alt=''></button>
+                    <span id='eDislikes`+ numEvents +`'>0</span>
+                    <button type='button' name='button' onclick='eDownVote(`+ numEvents +`)'><img class='thumb' src='./pics/Dislike.png' alt=''></button>
+                    <a role="button" class="collapsed" data-toggle="collapse" href="#ecollapse`+ numEvents +`" aria-expanded="false" onclick="ecolspand(`+ numEvents +`)" aria-controls="collapseExample"><img id="eexpand`+ numEvents +`" src="./pics/collapse.png" alt=""></a>
                 </div>
             </div>
-            <p>`+ post['description'] +`</p>
-
-            <p>ENDS: `+ post['endDate'] +`<br>COST: `+ post['cost'] +`<br> LOCATION: `+ post['location'] +` <br>TAGS: `+ post['tags'] +`</p>`;
+            <span class="collapse" id="ecollapse`+ numEvents +`" aria-expanded="false">
+                <p>`+ post['description'] +`</p>
+                <p>ENDS: `+ post['endDate'] +`<br>COST: `+ post['cost'] +`<br> LOCATION: `+ post['location'] +` <br>TAGS: `+ post['tags'] +`</p>
+            </span>`;
         document.getElementById('eventUL').appendChild(div);
     }
 }
@@ -198,4 +202,22 @@ function ecolspand(num) {
         document.getElementById(id).src = "./pics/collapse.png"
     }
 
+}
+
+function dfavorite(num){
+    var id = 'dfavorite' + num;
+    if(document.getElementById(id).src == "file:///Users/ethantom/Documents/School/CMPM%20131/pics/fav.png") {
+        document.getElementById(id).src = "./pics/faved.png";
+    } else {
+        document.getElementById(id).src = "./pics/fav.png"
+    }
+}
+
+function efavorite(num){
+    var id = 'efavorite' + num;
+    if(document.getElementById(id).src == "file:///Users/ethantom/Documents/School/CMPM%20131/pics/fav.png") {
+        document.getElementById(id).src = "./pics/faved.png";
+    } else {
+        document.getElementById(id).src = "./pics/fav.png"
+    }
 }
