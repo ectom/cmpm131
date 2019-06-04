dddad9ee66180771deccbece1d06678774ff31bc
Ethan Tom <etom579@gmail.com>
2019-06-03 17:23:07 -0700

    submissions can now be added successfully. still needs favorite button and collapsing functions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+var numDeals = 3;
+var numEvents = 2;
+
 // opens popups
 function openForm() {
     document.getElementById("filterForm").style.display = "block";
@@ -8,15 +11,15 @@ function closeForm() {
     document.getElementById("filterForm").style.display = "none";
 }
 
-function upVote(a){
-    y = "likes"+a
+function upVote(a) {
+    y = "likes" + a
     var x = document.getElementById(y).innerHTML;
     x++;
     document.getElementById(y).innerHTML = x;
 }
 
-function downVote(a){
-    y = "dislikes"+a
+function downVote(a) {
+    y = "dislikes" + a
     var x = document.getElementById(y).innerHTML;
     x++;
     document.getElementById(y).innerHTML = x;
@@ -64,18 +67,102 @@ function searchEvents() {
     }
 }
 
-// This script should make sure something is filled out for submissions. It doesn't
-function required(){
-    var empt = document.forms["postTitle"]["postDescription"]["type"].value;
-    if (empt == "")
-    {
-        alert("Please fill in every category");
-    return false;
-    }
+function addPost(post){
+    var div = document.createElement('div');
+    div.className = "post"
 
-    else
-    {
-    alert('Congrats on finishing your submission! Our editors will review it soon.');
-    return true;
+    if(post["postType"] == "deal") {
+        numDeals++;
+        div.innerHTML = `
+            <div class='post_title'>
+                <h3>`+ post['title'] +`</h3>
+                <div class='clickables'>
+                    <img class='verify' src='./pics/verify.png' alt=''>
+                    <span id='likes`+ numDeals +`'>0</span>
+                    <button onclick='upVote(`+ numDeals +`)'><img class='thumb' src='./pics/Like.png' alt=''></button>
+                    <span id='dislikes`+ numDeals +`'>0</span>
+                    <button type='button' name='button' onclick='downVote(`+ numDeals +`)'><img class='thumb' src='./pics/Dislike.png' alt=''></button>
+                </div>
+            </div>
+            <p>`+ post['description'] +`</p>
+
+            <p>ENDS: `+ post['endDate'] +`<br>COST: `+ post['cost'] +`<br> LOCATION: `+ post['location'] +` <br>TAGS: `+ post['tags'] +`</p>`;
+            document.getElementById('dealUL').appendChild(div);
+    } else {
+        numEvents++;
+        div.innerHTML = `
+            <div class='post_title'>
+                <h3>`+ post['title'] +`</h3>
+                <div class=''clickables''>
+                    <img class='verify' src='./pics/verify.png' alt=''>
+                    <span id='likes`+ numEvents +`'>0</span>
+                    <button onclick='upVote(`+ numEvents +`)'><img class='thumb' src='./pics/Like.png' alt=''></button>
+                    <span id='dislikes`+ numEvents +`'>0</span>
+                    <button type='button' name='button' onclick='downVote(`+ numEvents +`)'><img class='thumb' src='./pics/Dislike.png' alt=''></button>
+                </div>
+            </div>
+            <p>`+ post['description'] +`</p>
+
+            <p>ENDS: `+ post['endDate'] +`<br>COST: `+ post['cost'] +`<br> LOCATION: `+ post['location'] +` <br>TAGS: `+ post['tags'] +`</p>`;
+        document.getElementById('eventUL').appendChild(div);
+    }
+}
+
+// This script should make sure something is filled out for submissions. It doesn't
+function required() {
+    if (document.getElementById("postTitle").value == "" || document.getElementById("postDescription").value == "" || document.getElementById("tags").value == "" || document.getElementById("location").value == "") {
+        alert("Please fill in every category");
+    } else {
+        var post = {
+            'title': '',
+            'startDate': '',
+            'endDate': '',
+            'location': '',
+            'description': '',
+            'postType': '',
+            'tags': '',
+            'cost': ''
+        };
+
+        // get data from form
+        post['title'] = document.getElementById("postTitle").value;
+        post['startDate'] = document.getElementById("startDate").value;
+        post['endDate'] = document.getElementById("endDate").value;
+        post['location'] = document.getElementById("location").value;
+        post['description'] = document.getElementById("postDescription").value;
+        var postTypes = document.getElementsByName("postType");
+        for(var i = 0; i < postTypes.length; i++){
+            if(postTypes[i].checked){
+                post['postType'] = postTypes[i].value;
+            }
+        }
+        post['tags'] = document.getElementById("tags").value;
+        var costs = document.getElementsByName("cost");
+        for(var i = 0; i < costs.length; i++){
+            if(costs[i].checked){
+                post['cost'] = costs[i].value;
+            }
+        }
+        console.log("post: ", post);
+
+        // clear data from form
+        document.getElementById("postTitle").value = "";
+        document.getElementById("startDate").value = "";
+        document.getElementById("endDate").value = "";
+        document.getElementById("location").value = "";
+        document.getElementById("postDescription").value = "";
+        for(var i = 0; i < postTypes.length; i++){
+            if(postTypes[i].checked){
+                document.getElementsByName("postType")[i].checked = false;
+            }
+        }
+        $("#tags").tagsinput('removeAll');
+        for(var i = 0; i < costs.length; i++){
+            if(costs[i].checked){
+                document.getElementsByName("cost")[i].checked = false;
+            }
+        }
+
+        addPost(post);
     }
 }
